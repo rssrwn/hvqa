00181c26119725c8249931f97860e59e2f7fb7a9
rssrwn <rssrwn@gmail.com>
2020-02-03 13:13:22 +0000

    Add classification evaluator class, allow classification as flag to eval script

--- hvqa/detection/evaluator.py
+++ hvqa/detection/evaluator.py
@@ -1,24 +1,41 @@
 import torch
+import torch.nn.functional as F
+from torch.utils.data.dataloader import DataLoader
 from pathlib import Path
 from shapely.geometry import Polygon
 from PIL import ImageDraw
 import numpy as np
 
 from hvqa.util import build_data_loader, load_model, extract_bbox_and_class
-from hvqa.detection.dataset import DetectionDataset
-from hvqa.detection.model import DetectionModel
+from hvqa.detection.dataset import DetectionDataset, ClassifierDataset
+from hvqa.detection.model import DetectionModel, MyResNet
 
 
-class DetectionEvaluator:
+class _AbsEvaluator:
     def __init__(self, test_data_dir):
+        self.test_data_dir = test_data_dir
+
+    def eval_models(self, model_dir, conf_threshold=0.5):
+        model_path = Path(model_dir)
+        if model_path.is_dir():
+            print("Model path is a directory, evaluating all models in dir...")
+            for model_file in model_path.iterdir():
+                self.eval_model(model_file, conf_threshold)
+        else:
+            print("Model path is a file, evaluating single model...")
+            self.eval_model(model_dir, conf_threshold)
+
+    def eval_model(self, model_file, threshold):
+        raise NotImplementedError()
+
+
+class DetectionEvaluator(_AbsEvaluator):
+    def __init__(self, test_data_dir):
+        super(DetectionEvaluator, self).__init__(test_data_dir)
         batch_size = 1
         self.test_loader = build_data_loader(DetectionDataset, test_data_dir, batch_size)
 
-    def eval_models(self, model_dir, conf_threshold=0.5):
-        for model_file in Path(model_dir).iterdir():
-            self.eval_model(model_file, conf_threshold)
-
-    def eval_model(self, model_file, conf_threshold=0.5):
+    def eval_model(self, model_file, threshold=0.5):
         model = load_model(DetectionModel, Path(model_file))
         model.eval()
 
@@ -27,8 +44,8 @@ class DetectionEvaluator:
             with torch.no_grad():
                 preds = model(x)
 
-            preds_arr = extract_bbox_and_class(preds[0, :, :, :], conf_threshold)
-            actual_arr = extract_bbox_and_class(y[0, :, :, :], conf_threshold)
+            preds_arr = extract_bbox_and_class(preds[0, :, :, :], threshold)
+            actual_arr = extract_bbox_and_class(y[0, :, :, :], threshold)
 
             for actual_obj in actual_arr:
                 best_iou = None
@@ -91,3 +108,44 @@ class DetectionEvaluator:
             x2 += 1
             y2 += 1
             drawer.rectangle((x1, y1, x2, y2), fill=None, outline=colour)
+
+
+class ClassificationEvaluator(_AbsEvaluator):
+    def __init__(self, test_data_dir):
+        super(ClassificationEvaluator, self).__init__(test_data_dir)
+        batch_size = 48
+        img_size = 128
+        dataset = ClassifierDataset(test_data_dir, img_size)
+        self.test_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+    def eval_model(self, model_file, threshold=0.7):
+        model = load_model(MyResNet, Path(model_file))
+        model.eval()
+
+        print(f"Evaluating model at {model_file} ...")
+
+        mses = torch.tensor([], dtype=torch.float32)
+        num_correct = torch.tensor([], dtype=torch.float32)
+        for i, (x, y) in enumerate(self.test_loader):
+            with torch.no_grad():
+                preds = model(x)
+
+            num_predictions = preds.shape[1]
+
+            # Calculate MSE
+            mse = F.mse_loss(preds, y, reduction="none")
+            mse_sum = torch.sum(mse, 1)
+            mses = torch.cat((mses, mse_sum))
+
+            # Calculate number correct predictions
+            act_bool = y == 1
+            preds_bool = preds > threshold
+            correct = act_bool == preds_bool
+            num_correct_batch = torch.sum(correct, 1).type(torch.float32)
+            num_correct = torch.cat((num_correct, num_correct_batch))
+
+        assert mses.shape[0] == num_correct.shape[0], "Different numbers of images seen"
+
+        print(f"Avg MSE: {torch.mean(mses)}")
+        print(f"Avg correct predictions: {torch.mean(num_correct)}")
+        print(f"Avg prediction accuracy: {torch.mean(num_correct) / num_predictions}")
--- hvqa/scripts/evaluate.py
+++ hvqa/scripts/evaluate.py
@@ -1,24 +1,37 @@
 import argparse
+from pathlib import Path
 
-from hvqa.detection.evaluator import DetectionEvaluator
+from hvqa.detection.evaluator import DetectionEvaluator, ClassificationEvaluator
 
 
 CONF_THRESHOLD = 0.5
 
 
-def main(test_dir, model_file, visualise):
-    evaluator = DetectionEvaluator(test_dir)
+def eval_classifier(evaluator, model_file):
+    evaluator.eval_models(model_file)
 
+
+def eval_detector(evaluator, model_file, visualise):
     if visualise:
         evaluator.visualise(model_file, CONF_THRESHOLD)
 
     # evaluator.eval_model(model_file, CONF_THRESHOLD)
 
 
+def main(test_dir, model_file, classifier, visualise):
+    if classifier:
+        evaluator = ClassificationEvaluator(test_dir)
+        eval_classifier(evaluator, model_file)
+    else:
+        evaluator = DetectionEvaluator(test_dir)
+        eval_detector(evaluator, model_file, visualise)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Evaluate performance of a trained object detection model")
     parser.add_argument("eval_dir", type=str)
     parser.add_argument("model_file", type=str)
+    parser.add_argument("-c", "--classifier", action="store_true", default=False)
     parser.add_argument("-v", "--visualise", action="store_true", default=False)
     args = parser.parse_args()
-    main(args.eval_dir, args.model_file, args.visualise)
+    main(args.eval_dir, args.model_file, args.classifier, args.visualise)
