af7c9c6cb8c26e1ec2a64f3dab75c08cc4f223a6
rssrwn <rssrwn@gmail.com>
2020-04-12 16:49:22 +0100

    Print info on each question type in evaluate

--- hvqa/evaluate.py
+++ hvqa/evaluate.py
@@ -10,9 +10,17 @@ EVENT_ASP_DIR = "hvqa/events"
 QA_ASP_DIR = "hvqa/qa"
 
 
+def _inc_in_map(coll, key):
+    val = coll.get(key)
+    if val is None:
+        coll[key] = 0
+
+    coll[key] += 1
+
+
 def evaluate(model, data):
-    total = 0
-    num_correct = 0
+    correct = {}
+    incorrect = {}
 
     for video_idx in range(len(data)):
         print(f"Running on video {video_idx}")
@@ -25,19 +33,33 @@ def evaluate(model, data):
 
         for idx, predicted in enumerate(answers):
             actual = expected[idx]
+            q_type = q_types[idx]
             if actual == predicted:
-                print("correct")
-                num_correct += 1
+                print(f"Q{idx}: correct")
+                _inc_in_map(correct, q_type)
             else:
-                print("incorrect")
+                print(f"Q{idx}: incorrect. Got: {predicted}, actual: {actual}")
+                _inc_in_map(incorrect, q_type)
 
-            total += 1
+    q_types = list(set(correct.keys()).union(set(incorrect.keys())))
+    sorted(q_types)
 
+    print(f"\n{'Question Type':<20}{'Correct':<15}{'Incorrect':<15}Accuracy")
+    for q_type in q_types:
+        num_correct = correct.get(q_type)
+        num_correct = 0 if num_correct is None else num_correct
+        num_incorrect = incorrect.get(q_type)
+        num_incorrect = 0 if num_incorrect is None else num_incorrect
+        acc = (num_correct / (num_correct + num_incorrect)) * 100
+        print(f"{q_type:<20}{num_correct:<15}{num_incorrect:<15}{acc:.3g}%")
+
+    num_correct = sum(correct.values())
+    total = num_correct + sum(incorrect.values())
     acc = (num_correct / total) * 100
 
-    print(f"Num correct: {num_correct}")
+    print(f"\nNum correct: {num_correct}")
     print(f"Total: {total}")
-    print(f"Accuracy: {acc:.2}%")
+    print(f"Accuracy: {acc:.3g}%")
 
 
 def main(data_dir, model_type):
