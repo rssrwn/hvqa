66d368e84571eb188185d627fe3a449a06aaf79f
rssrwn <rssrwn@gmail.com>
2020-04-20 19:12:33 +0100

    Use id instead of idx for testing

--- hvqa/test/obj_tracker_test.py
+++ hvqa/test/obj_tracker_test.py
@@ -4,9 +4,9 @@ from hvqa.util.video_repr import Obj
 from hvqa.tracking.obj_tracker import ObjTracker
 
 
-obj1 = Obj("rock", (30, 30, 60, 60))
-obj1_copy = Obj("rock", (30, 30, 60, 60))
-obj1_similar = Obj("rock", (35, 35, 65, 65))
+obj1 = Obj("octopus", (30, 30, 60, 60))
+obj1_copy = Obj("octopus", (30, 30, 60, 60))
+obj1_similar = Obj("octopus", (45, 30, 75, 60))
 obj2 = Obj("fish", (100, 30, 200, 200))
 obj3 = Obj("octopus", (110, 40, 0, 0))
 obj4 = Obj("fish", (30, 30, 60, 60))
@@ -16,6 +16,7 @@ obj5 = Obj("rock", (110, 40, 0, 0))
 class ObjTrackerTest(unittest.TestCase):
     def setUp(self):
         self.tracker = ObjTracker()
+        self.tracker_err_corr = ObjTracker(err_corr=True)
 
     def test_dist_is_zero(self):
         dist = self.tracker.dist(obj1, obj1)
@@ -60,48 +61,51 @@ class ObjTrackerTest(unittest.TestCase):
 
     def test_process_frame_initial_frame(self):
         objs = [obj1, obj2, obj3, obj4, obj5]
-        exp_idxs = [0, 1, 2, 3, 4]
+        exp_ids = [0, 1, 2, 3, 4]
         self.tracker.process_frame_(objs)
-        idxs = [obj.id for obj in objs]
-        self.assertEqual(exp_idxs, idxs)
+        ids = [obj.id for obj in objs]
+        self.assertEqual(exp_ids, ids)
 
     def test_process_frame_no_change(self):
         objs = [obj1, obj2, obj3, obj4, obj5]
-        exp_idxs = [0, 1, 2, 3, 4]
+        exp_ids = [0, 1, 2, 3, 4]
         for _ in range(2):
             self.tracker.process_frame_(objs)
-            idxs = [obj.id for obj in objs]
-            self.assertEqual(exp_idxs, idxs)
+            ids = [obj.id for obj in objs]
+            self.assertEqual(exp_ids, ids)
 
     def test_process_frame_moved_obj(self):
         objs1 = [obj2, obj3, obj4, obj5, obj1]
         objs2 = [obj2, obj3, obj1_similar, obj4, obj5]
-        exp_idxs = [[0, 1, 2, 3, 4], [0, 1, 4, 2, 3]]
-        idxs = self._process_frames([objs1, objs2])
-        self.assertEqual(exp_idxs, idxs)
+        exp_ids = [[0, 1, 2, 3, 4], [0, 1, 4, 2, 3]]
+        ids = self._process_frames([objs1, objs2])
+        self.assertEqual(exp_ids, ids)
 
     def test_process_frame_new_obj(self):
         objs1 = [obj2, obj3, obj4, obj5]
         objs2 = [obj2, obj3, obj4, obj1, obj5]
-        exp_idxs = [[0, 1, 2, 3], [0, 1, 2, 4, 3]]
-        idxs = self._process_frames([objs1, objs2])
-        self.assertEqual(exp_idxs, idxs)
+        exp_ids = [[0, 1, 2, 3], [0, 1, 2, 4, 3]]
+        ids = self._process_frames([objs1, objs2])
+        self.assertEqual(exp_ids, ids)
 
     def test_process_frame_disappear_obj(self):
         objs1 = [obj1, obj2, obj3, obj4, obj5]
         objs2 = [obj2, obj3, obj4, obj5]
         objs3 = [obj2, obj3, obj4, obj5]
-        exp_idxs = [[0, 1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]]
-        idxs = self._process_frames([objs1, objs2, objs3])
-        self.assertEqual(exp_idxs, idxs)
+        exp_ids = [[0, 1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]]
+        ids = self._process_frames([objs1, objs2, objs3])
+        self.assertEqual(exp_ids, ids)
 
     def test_process_frame_hidden_obj(self):
         objs1 = [obj1, obj2, obj3, obj4, obj5]
         objs2 = [obj2, obj3, obj4, obj5]
         objs3 = [obj2, obj3, obj4, obj5, obj1]
-        exp_idxs = [[0, 1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4, 0]]
-        idxs = self._process_frames([objs1, objs2, objs3])
-        self.assertEqual(exp_idxs, idxs)
+        exp_ids = [[0, 1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4, 0]]
+        ids = self._process_frames([objs1, objs2, objs3])
+        self.assertEqual(exp_ids, ids)
+
+    # def test_err_correction(self):
+
 
     def _process_frames(self, frames):
         idxs = []
