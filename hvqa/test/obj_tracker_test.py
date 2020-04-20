04204fba20294bd4b52c7e6ef577e6d4aa54d183
rssrwn <rssrwn@gmail.com>
2020-04-20 19:19:52 +0100

    Add tests for error correction tracker

--- hvqa/test/obj_tracker_test.py
+++ hvqa/test/obj_tracker_test.py
@@ -6,7 +6,9 @@ from hvqa.tracking.obj_tracker import ObjTracker
 
 obj1 = Obj("octopus", (30, 30, 60, 60))
 obj1_copy = Obj("octopus", (30, 30, 60, 60))
-obj1_similar = Obj("octopus", (45, 30, 75, 60))
+obj1_err = Obj("octopus", (31, 30, 61, 60))
+obj1_moved = Obj("octopus", (45, 30, 75, 60))
+obj1_err_moved = Obj("octopus", (46, 30, 76, 60))
 obj2 = Obj("fish", (100, 30, 200, 200))
 obj3 = Obj("octopus", (110, 40, 0, 0))
 obj4 = Obj("fish", (30, 30, 60, 60))
@@ -36,13 +38,13 @@ class ObjTrackerTest(unittest.TestCase):
         self.assertFalse(is_close, "A difference of more than 10 should not be close")
 
     def test_find_best_match(self):
-        objs = enumerate([obj1_similar, obj1_copy, obj2, obj3])
+        objs = enumerate([obj1_moved, obj1_copy, obj2, obj3])
         exp_idx = 1
         idx = self.tracker._find_best_match(obj1, objs)
         self.assertEqual(exp_idx, idx, "Found wrong match for obj")
 
     def test_find_best_match_moved(self):
-        objs = enumerate([obj2, obj3, obj1_similar])
+        objs = enumerate([obj2, obj3, obj1_moved])
         exp_idx = 2
         idx = self.tracker._find_best_match(obj1, objs)
         self.assertEqual(exp_idx, idx, "Found wrong match after obj moved")
@@ -76,7 +78,7 @@ class ObjTrackerTest(unittest.TestCase):
 
     def test_process_frame_moved_obj(self):
         objs1 = [obj2, obj3, obj4, obj5, obj1]
-        objs2 = [obj2, obj3, obj1_similar, obj4, obj5]
+        objs2 = [obj2, obj3, obj1_moved, obj4, obj5]
         exp_ids = [[0, 1, 2, 3, 4], [0, 1, 4, 2, 3]]
         ids = self._process_frames([objs1, objs2])
         self.assertEqual(exp_ids, ids)
@@ -104,8 +106,22 @@ class ObjTrackerTest(unittest.TestCase):
         ids = self._process_frames([objs1, objs2, objs3])
         self.assertEqual(exp_ids, ids)
 
-    # def test_err_correction(self):
+    def test_err_corr_appear_disappear(self):
+        objs1 = [obj1, obj2, obj5]
+        objs2 = [obj1, obj1_err, obj2, obj5]
+        objs3 = [obj1, obj2, obj5]
+        exp_ids = [[0, 1, 2], [0, 0, 1, 2], [0, 1, 2]]
+        ids = self._process_frames([objs1, objs2, objs3])
+        self.assertEqual(exp_ids, ids)
 
+    def test_err_corr_move(self):
+        objs1 = [obj1, obj2, obj5]
+        objs2 = [obj1, obj1_err, obj2, obj5]
+        objs3 = [obj1_moved, obj1_err_moved, obj2, obj5]
+        objs4 = [obj1, obj2, obj5]
+        exp_ids = [[0, 1, 2], [0, 0, 1, 2], [0, 0, 1, 2], [0, 1, 2]]
+        ids = self._process_frames([objs1, objs2, objs3, objs4])
+        self.assertEqual(exp_ids, ids)
 
     def _process_frames(self, frames):
         idxs = []
