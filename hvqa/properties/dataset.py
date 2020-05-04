96a79cae7414ce5fe4dfe0b7de605cfd18667ab2
rssrwn <rssrwn@gmail.com>
2020-05-04 17:13:31 +0100

    Update QAPropDataset to return info on objs in frame for each frame given by property questions

--- hvqa/properties/dataset.py
+++ hvqa/properties/dataset.py
@@ -1,7 +1,4 @@
-import random
 from torch.utils.data import Dataset
-
-from hvqa.util.func import append_in_map
 
 
 class VideoPropDataset(Dataset):
@@ -50,123 +47,65 @@ class VideoPropDataset(Dataset):
 
 
 class QAPropDataset(Dataset):
-    def __init__(self, spec, videos, answers=None, transform=None):
-        """
-        Create a dataset for property extraction
-
-        :param spec: EnvSpec object
-        :param videos: List of Video objects
-        :param answers: List of list of answers ([[str]])
-        :param transform: Torchvision Transform to apply to PIL image of object
-        """
-
+    def __init__(self, spec, videos, answers, transform=None):
         self.spec = spec
+        self.videos = videos
         self.transform = transform
-        self.answers = answers
 
-        # This needs to be run after setting the others
-        self.obj_data, self.num_items = self._collect_data(videos)
+        assert len(videos) == len(answers), "There must be an equal number of videos and answer lists"
+        for idx, video in enumerate(videos):
+            assert_str = "Each video should have the same number of questions as answers"
+            assert len(video.questions) == len(answers[idx]), assert_str
 
-        super(QAPropDataset, self).__init__()
+        self.imgs, self.qa_classes, self.props = self._collect_data(videos, answers)
 
     def __len__(self):
-        return self.num_items
+        return len(self.imgs)
 
     def __getitem__(self, item):
-        targets = {prop: items[item] for prop, items in self.obj_data.items()}
-        targets = {prop: (self.transform(img), target) for prop, (img, target) in targets.items()}
-        return targets
+        imgs = self.imgs[item]
+        cls = self.qa_classes[item]
+        props = self.props[item]
+        if self.transform is not None:
+            imgs = [self.transform(img) for img in imgs]
 
-    def _collect_data(self, videos):
-        objs = {}
-        for video_idx, video in enumerate(videos):
-            questions = video.questions
-            q_types = video.q_types
-            answers = self.answers[video_idx]
-            frames = video.frames
+        return imgs, cls, props
 
-            # TODO put this in spec (or a new question spec obj)
+    @staticmethod
+    def from_video_dataset(spec, dataset, transform=None):
+        data = [dataset[idx] for idx in range(len(dataset))]
+        videos, answers = tuple(zip(*data))
+        prop_dataset = QAPropDataset(spec, videos, answers, transform=transform)
+        return prop_dataset
+
+    def _collect_data(self, videos, answers):
+        imgs = []
+        cls = []
+        props = []
+        for idx, video in enumerate(videos):
+            video_ans = answers[idx]
+
+            # TODO Move this
             prop_q_type = 0
+            q_idxs = [idx for idx, q_type in video.q_types if q_type == prop_q_type]
 
-            q_idxs = [idx for idx, q_type in enumerate(q_types) if q_type == prop_q_type]
             for q_idx in q_idxs:
-                question = questions[q_idx]
-                answer = answers[q_idx]
-                obj_tuple = self._collect_obj(frames, question, answer)
-                if obj_tuple is not None:
-                    obj_cls, img, targets = obj_tuple
-                    append_in_map(objs, obj_cls, (img, targets))
+                question = video.questions[q_idx]
+                ans = video_ans[q_idx]
+                prop, val, cls, frame_idx = self._parse_question(question)
 
-        print(f"{'Class':<15}{'Num Objs'}")
-        for cls, items in objs.items():
-            print(f"{cls:<15}{len(items)}")
-
-        # Sample from each class equally
-        sampled_data = []
-        items_per_cls = max([len(items) for cls, items in objs.items()])
-        for cls, items in objs.items():
-            if len(items) == items_per_cls:
-                sampled_data.extend(items)
-            else:
-                idxs = random.choices(range(len(items)), k=items_per_cls)
-                sampled = [items[idx] for idx in idxs]
-                sampled_data.extend(sampled)
-
-        prop_map = {}
-        for img, targets in sampled_data:
-            for prop, val in targets.items():
-                append_in_map(prop_map, prop, (img, {prop: val}))
-
-        # Sample from each property equally
-        sampled_prop_map = {}
-        num_items = max([len(items) for prop, items in prop_map.items()])
-        for prop, items in prop_map.items():
-            if len(items) == num_items:
-                sampled_prop_map[prop] = items
-            else:
-                sampled_prop_map[prop] = random.choices(items, k=num_items)
-
-        return sampled_prop_map, num_items
-
-    def _collect_obj(self, frames, question, answer):
-        objs = []
-        prop, val, cls, frame_idx = self._parse_question(question)
-        frame = frames[frame_idx]
-        for obj in frame.objs:
-            obj_val = None
-            if val is not None:
-                prop_for_val = self.spec.find_prop(val)
-                obj_val = obj.prop_vals.get(prop_for_val)
-
-            # Match an obj (if val is None we can still match if data has not been loaded)
-            if obj.cls == cls and val == obj_val:
-
-                # TODO remove
-                if prop == "rotation":
-                    try:
-                        answer = int(answer)
-                    except ValueError:
-                        print(f"Question: {question} -- answer: {answer}")
-                        continue
-
-                    answer = self.spec.from_internal(prop, int(answer))
-
-                target = {prop: answer}
-
-                # Add the val given in the question as well (if we can)
+                frame = video.frames[frame_idx]
+                frame_imgs = [obj.img for obj in frame.objs]
+                q_props = {prop: ans}
                 if val is not None:
-                    target[prop_for_val] = val
+                    val_prop = self.spec.find_prop(val)
+                    q_props[val_prop] = val
 
-                objs.append((cls, obj.img, target))
+                imgs.append(frame_imgs)
+                cls.append(cls)
+                props.append(q_props)
 
-        if len(objs) == 0:
-            return None
-
-        obj = objs[0]
-        if len(objs) > 1:
-            print(f"WARNING: Found multiple objects for frame {frame_idx}, question {question}")
-
-        return obj
+        return imgs, cls, props
 
     # TODO create separate question parsing class
     def _parse_question(self, question):
@@ -182,3 +121,138 @@ class QAPropDataset(Dataset):
             cls = splits[5]
 
         return prop, val, cls, frame_idx
+
+
+# class QAPropDataset(Dataset):
+#     def __init__(self, spec, videos, answers=None, transform=None):
+#         """
+#         Create a dataset for property extraction
+#
+#         :param spec: EnvSpec object
+#         :param videos: List of Video objects
+#         :param answers: List of list of answers ([[str]])
+#         :param transform: Torchvision Transform to apply to PIL image of object
+#         """
+#
+#         self.spec = spec
+#         self.transform = transform
+#         self.answers = answers
+#
+#         # This needs to be run after setting the others
+#         self.obj_data, self.num_items = self._collect_data(videos)
+#
+#         super(QAPropDataset, self).__init__()
+#
+#     def __len__(self):
+#         return self.num_items
+#
+#     def __getitem__(self, item):
+#         targets = {prop: items[item] for prop, items in self.obj_data.items()}
+#         targets = {prop: (self.transform(img), target) for prop, (img, target) in targets.items()}
+#         return targets
+#
+#     def _collect_data(self, videos):
+#         objs = {}
+#         for video_idx, video in enumerate(videos):
+#             questions = video.questions
+#             q_types = video.q_types
+#             answers = self.answers[video_idx]
+#             frames = video.frames
+#
+#             # TODO put this in spec (or a new question spec obj)
+#             prop_q_type = 0
+#
+#             q_idxs = [idx for idx, q_type in enumerate(q_types) if q_type == prop_q_type]
+#             for q_idx in q_idxs:
+#                 question = questions[q_idx]
+#                 answer = answers[q_idx]
+#                 obj_tuple = self._collect_obj(frames, question, answer)
+#                 if obj_tuple is not None:
+#                     obj_cls, img, targets = obj_tuple
+#                     append_in_map(objs, obj_cls, (img, targets))
+#
+#         print(f"{'Class':<15}{'Num Objs'}")
+#         for cls, items in objs.items():
+#             print(f"{cls:<15}{len(items)}")
+#
+#         # Sample from each class equally
+#         sampled_data = []
+#         items_per_cls = max([len(items) for cls, items in objs.items()])
+#         for cls, items in objs.items():
+#             if len(items) == items_per_cls:
+#                 sampled_data.extend(items)
+#             else:
+#                 idxs = random.choices(range(len(items)), k=items_per_cls)
+#                 sampled = [items[idx] for idx in idxs]
+#                 sampled_data.extend(sampled)
+#
+#         prop_map = {}
+#         for img, targets in sampled_data:
+#             for prop, val in targets.items():
+#                 append_in_map(prop_map, prop, (img, {prop: val}))
+#
+#         # Sample from each property equally
+#         sampled_prop_map = {}
+#         num_items = max([len(items) for prop, items in prop_map.items()])
+#         for prop, items in prop_map.items():
+#             if len(items) == num_items:
+#                 sampled_prop_map[prop] = items
+#             else:
+#                 sampled_prop_map[prop] = random.choices(items, k=num_items)
+#
+#         return sampled_prop_map, num_items
+#
+#     def _collect_obj(self, frames, question, answer):
+#         objs = []
+#         prop, val, cls, frame_idx = self._parse_question(question)
+#         frame = frames[frame_idx]
+#         for obj in frame.objs:
+#             obj_val = None
+#             if val is not None:
+#                 prop_for_val = self.spec.find_prop(val)
+#                 obj_val = obj.prop_vals.get(prop_for_val)
+#
+#             # Match an obj (if val is None we can still match if data has not been loaded)
+#             if obj.cls == cls and val == obj_val:
+#
+#                 # TODO remove
+#                 if prop == "rotation":
+#                     try:
+#                         answer = int(answer)
+#                     except ValueError:
+#                         print(f"Question: {question} -- answer: {answer}")
+#                         continue
+#
+#                     answer = self.spec.from_internal(prop, int(answer))
+#
+#                 target = {prop: answer}
+#
+#                 # Add the val given in the question as well (if we can)
+#                 if val is not None:
+#                     target[prop_for_val] = val
+#
+#                 objs.append((cls, obj.img, target))
+#
+#         if len(objs) == 0:
+#             return None
+#
+#         obj = objs[0]
+#         if len(objs) > 1:
+#             print(f"WARNING: Found multiple objects for frame {frame_idx}, question {question}")
+#
+#         return obj
+#
+#     # TODO create separate question parsing class
+#     def _parse_question(self, question):
+#         splits = question.split(" ")
+#         prop = splits[1]
+#         frame_idx = int(splits[-1][:-1])
+#
+#         # Assume only one property value given in question
+#         cls = splits[4]
+#         val = None
+#         if cls not in self.spec.obj_types():
+#             val = cls
+#             cls = splits[5]
+#
+#         return prop, val, cls, frame_idx
