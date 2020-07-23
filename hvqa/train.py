2fd4ba0389c51a4e6d2beb9f284be71091554553
rssrwn <rssrwn@gmail.com>
2020-07-23 15:58:08 +0100

    Only extract objects if necessary

--- hvqa/train.py
+++ hvqa/train.py
@@ -1,6 +1,6 @@
 import argparse
 
-from hvqa.util.dataset import VideoDataset
+from hvqa.util.dataset import VideoDataset, BaselineDataset
 from hvqa.spec.env import EnvSpec
 from hvqa.detection.detector import NeuralDetector
 from hvqa.models.hardcoded import HardcodedVQAModel
@@ -28,22 +28,25 @@ spec = EnvSpec.from_dict({
 
 
 def main(train_dir, eval_dir, model_type):
-    # Create data
     detector = NeuralDetector.load(spec, DETECTOR_PATH)
-    train_data = VideoDataset.from_data_dir(spec, train_dir, detector, hardcoded=False)
-    eval_data = VideoDataset.from_data_dir(spec, eval_dir, detector, hardcoded=True)
 
     if model_type == "hardcoded":
         model_path = HARDCODED_MODEL_PATH
         model = HardcodedVQAModel.new(spec)
+        train_data = VideoDataset.from_data_dir(spec, train_dir, detector, hardcoded=False)
+        eval_data = VideoDataset.from_data_dir(spec, eval_dir, detector, hardcoded=True)
 
     elif model_type == "ind-trained":
         model_path = IND_MODEL_PATH
         model = IndTrainedModel.new(spec)
+        train_data = VideoDataset.from_data_dir(spec, train_dir, detector, hardcoded=False)
+        eval_data = VideoDataset.from_data_dir(spec, eval_dir, detector, hardcoded=True)
 
     elif model_type == "best-choice":
         model_path = BEST_CHOICE_MODEL_PATH
         model = BestChoiceModel(spec)
+        train_data = BaselineDataset.from_data_dir(train_dir)
+        eval_data = BaselineDataset.from_data_dir(eval_dir)
 
     else:
         print("That model type is not supported")
