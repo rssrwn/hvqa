93ee18ad949b3f08b468fdc0f6a17310dcc3934d
rssrwn <rssrwn@gmail.com>
2020-07-23 15:36:38 +0100

    Add BestChoiceModel class and add training function for this class

--- hvqa/models/baselines/question.py
+++ hvqa/models/baselines/question.py
@@ -8,7 +8,8 @@ NUM_Q_TYPES = 7
 
 
 class _AbsBaselineModel(BaselineModel):
-    def __init__(self):
+    def __init__(self, spec):
+        self.spec = spec
         self._reset_results()
 
     def _reset_results(self):
@@ -70,8 +71,7 @@ class _AbsBaselineModel(BaselineModel):
 
 class RandomAnsModel(_AbsBaselineModel):
     def __init__(self, spec):
-        self.spec = spec
-        super(RandomAnsModel, self).__init__()
+        super(RandomAnsModel, self).__init__(spec)
 
     def train(self, train_data, eval_data, verbose=True):
         pass
@@ -122,6 +122,72 @@ class RandomAnsModel(_AbsBaselineModel):
             raise UnknownQuestionTypeException(f"Question type {q_type} unknown")
 
         return ans
+
+    @staticmethod
+    def load(spec, path):
+        pass
+
+    def save(self, path):
+        pass
+
+
+class BestChoiceModel(_AbsBaselineModel):
+    def __init__(self, spec):
+        self._answers = None
+        super(BestChoiceModel, self).__init__(spec)
+
+    def train(self, train_data, eval_data, verbose=True):
+        counts = {}
+
+        num_vs = len(train_data)
+        for idx in range(num_vs):
+            _, qs, q_types, answers = train_data[idx]
+
+            for q_idx, question in enumerate(qs):
+                q_type = q_types[q_idx]
+                answer = answers[q_idx]
+                self._add_ans_count(counts, question, q_type, answer)
+
+        answers = {}
+        for q_type, ans_counts in counts.items():
+            if q_type == 0:
+                prop_answers = {}
+                for prop, val_counts in ans_counts.items():
+                    val, _ = max(val_counts.items, key=lambda val_cnt: val_cnt[1])
+                    prop_answers[prop] = val
+
+                answers[q_type] = prop_answers
+
+            else:
+                ans, _ = max(ans_counts.items, key=lambda ans_cnt: ans_cnt[1])
+                answers[q_type] = ans
+
+    def _add_ans_count(self, counts, question, q_type, answer):
+        q_type_counts = counts.get(q_type)
+        if q_type_counts is None:
+            counts[q_type] = {}
+
+        if q_type == 0:
+            prop, _, _, _ = self.spec.qa.parse_prop_question(question)
+            prop_counts = counts[q_type].get(prop)
+            if prop_counts is None:
+                counts[q_type][prop] = {}
+
+            ans_count = counts[q_type][prop].get(answer)
+            if ans_count is None:
+                counts[q_type][prop][answer] = 0
+
+            counts[q_type][prop][answer] += 1
+
+        else:
+            ans_count = counts[q_type].get(answer)
+            if ans_count is None:
+                counts[q_type][answer] = 0
+
+            counts[q_type][answer] += 1
+
+    def eval(self, eval_data, verbose=True):
+        pass
 
     @staticmethod
     def load(spec, path):
