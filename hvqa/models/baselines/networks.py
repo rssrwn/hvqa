8656353b79028cd1ad3934589fe38c7fd446bdea
rssrwn <rssrwn@gmail.com>
2020-08-19 12:43:55 +0100

    Cleanup

--- hvqa/models/baselines/networks.py
+++ hvqa/models/baselines/networks.py
@@ -1,9 +1,6 @@
 import torch
 import torch.nn as nn
 import torchvision.models as models
-from torch.nn.utils.rnn import pad_packed_sequence
-
-import hvqa.util.func as util
 
 
 # ------------------------------------------------------------------------------------------------------
